b778e117353bd6968f0602c2f480ea6941ab6d16
VovaTka4 <EpikBulldogg@mail.ru>
2024-11-03 20:53:43 +0300

    lab5 is done

--- LAB4/lunchscript.js
+++ LAB4/lunchscript.js
@@ -1,7 +1,9 @@
 const order = {
     soup: null,
     main: null,
-    drink: null
+    drink: null,
+    desert: null,
+    salad: null
 };
 
 function addToOrder(keyword) {
@@ -14,9 +16,15 @@ function addToOrder(keyword) {
     else if (selectedDish.category === 'main'){
         order.main = selectedDish;
         document.getElementById('main-selection').textContent = selectedDish.name;
-    } else{
+    } else if (selectedDish.category === 'drink'){
         order.drink = selectedDish;
         document.getElementById('drink-selection').textContent = selectedDish.name;
+    } else if (selectedDish.category === 'salad'){
+        order.salad = selectedDish;
+        document.getElementById('salad-selection').textContent = selectedDish.name;
+    } else if (selectedDish.category === 'desert'){
+        order.desert = selectedDish;
+        document.getElementById('desert-selection').textContent = selectedDish.name;
     }
 
     updateDisplay();
@@ -28,8 +36,10 @@ function updateDisplay(){
     const selectedSoup = document.getElementById('soup');
     const selectedMain = document.getElementById('main');
     const selectedDrink = document.getElementById('drink');
+    const selectedSalad = document.getElementById('salad');
+    const selectedDesert = document.getElementById('desert');
 
-    if (order.soup || order.drink || order.main){
+    if (order.soup || order.drink || order.main || order.desert || order.salad){
         noSelection.style.display = 'none';
         selectedSoup.style.display = 'block';
         selectedSoup.querySelector('span').textContent = order.soup ? order.soup.name+" "+order.soup.price+"₽" : 'Блюдо не выбрано';
@@ -37,11 +47,17 @@ function updateDisplay(){
         selectedMain.querySelector('span').textContent = order.main ? order.main.name+" "+order.main.price+"₽" : 'Блюдо не выбрано';
         selectedDrink.style.display = 'block';
         selectedDrink.querySelector('span').textContent = order.drink ? order.drink.name+" "+order.drink.price+"₽" : 'Напиток не выбран';
+        selectedSalad.querySelector('span').textContent = order.salad ? order.salad.name+" "+order.salad.price+"₽" : 'Блюдо не выбрано';
+        selectedSalad.style.display = 'block';
+        selectedDesert.querySelector('span').textContent = order.desert ? order.desert.name+" "+order.desert.price+"₽" : 'Блюдо не выбран';
+        selectedDesert.style.display = 'block';
         totalP.style.display = 'block';
         let total = 0;
         total += order.soup ? order.soup.price : 0;
         total += order.main ? order.main.price : 0;
         total += order.drink ? order.drink.price : 0;
+        total += order.salad ? order.salad.price : 0;
+        total += order.desert ? order.desert.price : 0;
         totalP.querySelector('span').textContent = `${total}₽`;
     }
     else{
@@ -50,6 +66,8 @@ function updateDisplay(){
         selectedSoup.style.display = 'none';
         selectedMain.style.display = 'none';
         selectedDrink.style.display = 'none';
+        selectedDesert.style.display = 'none';
+        selectedSalad.style.display = 'none';
     }
 }
 
@@ -58,6 +76,8 @@ function displayDish() {
         soup: document.querySelector('#soup-section .menu-container'),
         main: document.querySelector('#main-section .menu-container'),
         drink: document.querySelector('#drink-section .menu-container'),
+        salad: document.querySelector('#salad-section .menu-container'),
+        desert: document.querySelector('#desert-section .menu-container'),
     };
 
     dishes.sort((a,b) => a.name.localeCompare(b.name));
@@ -110,9 +130,13 @@ document.getElementById('postB').onclick = function(event){
     const soupForm = document.getElementById('hiddenSoup');
     const mainForm = document.getElementById('hiddenMain');
     const drinkForm = document.getElementById('hiddenDrink');
+    const desertForm = document.getElementById('hiddenDesert');
+    const saladForm = document.getElementById('hiddenSalad');
     soupForm.value = order.soup ? order.soup.keyword : '';
     mainForm.value = order.main ? order.main.keyword : '';
     drinkForm.value = order.drink ? order.drink.keyword : '';
+    desertForm.value = order.desert ? order.desert.keyword : '';
+    saladForm.value = order.salad ? order.salad.keyword : '';
 
     if (!soupForm.value || !mainForm.value || !drinkForm.value){
         event.preventDefault();
