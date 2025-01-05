c83948b2370f2691e7826b4abf18953668430d47
VovaTka4 <EpikBulldogg@mail.ru>
2025-01-05 22:41:35 +0300

    added notifications, added basket page, no functions

--- exam/mainpage.js
+++ exam/mainpage.js
@@ -1,6 +1,30 @@
 let items = [];
 let displayedItems = 0;
 
+function createNotification(text) {
+    const notificationContainer = document.getElementById('notifications');
+
+    const notsBox = document.createElement('div');
+    notsBox.classList.add('notification-box');
+    
+    const notsText = document.createElement('span');
+    notsText.classList.add('notification-text');
+    notsText.textContent = text;
+    notsBox.appendChild(notsText);
+    
+    const closeButton = document.createElement('span');
+    closeButton.classList.add('close-button');
+    closeButton.innerHTML = '×';
+
+    closeButton.onclick = function() {
+        notsBox.remove();
+    };
+    
+    notsBox.appendChild(closeButton);
+
+    notificationContainer.appendChild(notsBox);
+}
+
 function displayItems(howMuch) {
     const productGrid = document.getElementById('items');
 
@@ -98,6 +122,7 @@ async function loadItems() {
 
 document.getElementById('loadMore').onclick = function() {
     displayItems(8);
+    createNotification("Подгрузил!");
 };
 
 document.addEventListener("DOMContentLoaded", () => {
